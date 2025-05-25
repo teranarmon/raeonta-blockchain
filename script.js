4b0eeb21e3b402db6e9c32df66a04b2e4f49534e
teranarmon <raeonta2025@gmail.com>
2025-05-25 07:26:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-fetch("/api/chain")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("chain").innerHTML =
-      "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
-  });
